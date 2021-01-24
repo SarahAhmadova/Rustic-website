1f57b2dede59b20f1a0d8cc9130b698d8b05490d
SaraAhmadova <sara.ehmedova7@gmail.com>
2021-01-24 19:30:13 +0400

    side-bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,29 @@ $(document).ready(function () {
     });
 
     carouselContent($("#banner-slide"));
+    $("#sidebar .side-menu li>a").click((e) => {
+        if ($(e.currentTarget).parent().children().length > 1) {
+            $(e.currentTarget).parent().find(".inner-menu").toggle();
+            $(e.currentTarget).parent().find("span").toggleClass("flaticon-angle-arrow-down");
+            $(e.currentTarget).parent().find("span").toggleClass("flaticon-up-arrow-1");
+        }
+    })
+    $("#sidebar .close-btn").click(() => {
+        $("#sidebar").hide()
+        $("#sidebar").css("left", "100%");
 
+    })
+    $("#sidebar").click((e) => {
+        if ($(e.target).is("#sidebar")) {
+            $(e.target).hide()
+            $(e.target).css("left", "100%");
+        }
+
+    })
+    $(".navbar-toggler").click(() => {
+        $("#sidebar").show();
+        $("#sidebar").css("left", "0");
+    })
     $("#banner-slide").on(
         'translated.owl.carousel',
         function () {
@@ -49,7 +71,7 @@ $(document).ready(function () {
         autoplay: true,
         autoplayTimeout: 4000,
         autoplayHoverPause: true,
-        nav:true,
+        nav: true,
         dots: false,
         margin: 15,
         responsive: {
@@ -93,17 +115,22 @@ $(document).ready(function () {
     $(".form-switch").find("input").change(function () {
         $("#pricing .package-choice .frst").toggleClass("active");
         $("#pricing .package-choice .scnd").toggleClass("active");
-        $("#pricing .item").css("animation","none");
+        $("#pricing .item").css("animation", "none");
         $("#pricing .item").css("animation");
         $("#pricing .item").css({
             "animation": "slideUp 1s"
         });
-        
+
 
     });
 });
 
-
+$(window).resize(function () {
+    console.log("worked");
+    waitForFinalEvent(function(){
+        carouselContent($("#banner-slide"));
+      }, 500, "some unique string");
+});
 $(document).scroll(function () {
     if ($(this).scrollTop() > 90) {
         $("header").addClass("sticky-nav");
@@ -118,20 +145,31 @@ $(document).scroll(function () {
             $("#pricing .item").css({
                 animation: "slideUp 1s"
             });
-        }
-        else $("#pricing .item").css("animation", "none");
+        } else $("#pricing .item").css("animation", "none");
     });
 
 
 
 });
 
-
+let waitForFinalEvent = (function () {
+    let timers = {};
+    return function (callback, ms, uniqueId) {
+      if (!uniqueId) {
+        uniqueId = "Don't call this twice without a uniqueId";
+      }
+      if (timers[uniqueId]) {
+        clearTimeout (timers[uniqueId]);
+      }
+      timers[uniqueId] = setTimeout(callback, ms);
+    };
+  })();
 
 function carouselContent(e) {
     let item = $(e).find(".owl-item.active").find(".item");
     item.html("<div class='text-box'></div>");
     item.find(".text-box").append("<div class='sub-title'>nordic interior <br> studio</div><h1 class='title'>Rustic and <br> Modern</h1><a href='#' class='more-about'>more about us</a>");
+    item.append('            <a href="#" class="down-about"><span class="down-icon"></span><span class="down-icon"></span><span class="down-icon"></span>');
     AddContentAnime(e);
 }
 
